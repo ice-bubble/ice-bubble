0ac68e4f1d23403bbab7b00069a611a1e24becf6
ice-bubble <97901420+ice-bubble@users.noreply.github.com>
2022-01-18 21:56:45 +0800

    C++ 1st commit

---
+++ C++ Source/Test_File.cpp
@@ -0,0 +1,20 @@
+/*
+
+*/
+
+
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int main(void)
+{
+	char a[101];
+	cin.get(a,101);
+	cout << "Welcome to C++!";
+	cout << "\n";
+	cout << a;
+	cout << "\n";
+	system("pause");
+	return 0;
+}
---
+++ C++ Source/框架.cpp
@@ -0,0 +1,15 @@
+/*
+
+*/
+
+
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int main(void)
+{
+	
+	system("pause");
+	return 0;
+}
