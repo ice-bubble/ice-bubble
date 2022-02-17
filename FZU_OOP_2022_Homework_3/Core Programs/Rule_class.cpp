4a75e0e19207d8fd436dc1e851e565f385dccb40
ice-bubble <ice_snow@ice-bubble.com>
2022-02-17 19:52:19 +0800

    完成HiCuts算法基本框架，暂未恰当切分范围，导致优化不明显

--- FZU_OOP_2022_Homework_3/Core Programs/Rule_class.cpp
+++ FZU_OOP_2022_Homework_3/Core Programs/Rule_class.cpp
@@ -12,7 +12,7 @@ public:
 	class rule* create_chain_table(ifstream& fp_rule);
 	void process_chain_table(rule* p);
 	void visit_processed_chain_table(rule* p, ifstream& fp_packet, ofstream& fp_out);
-	class rule* Process_Rule_class(rule* p, unsigned int ip0min, unsigned int ip0max, unsigned int ip1min, unsigned int ip1max, int d01, int d02, int d11, int d12, int x0, int x1);
+	void process_rule_class(class rule** p, unsigned int ip0min, unsigned int ip0max, unsigned int ip1min, unsigned int ip1max, int d01, int d02, int d11, int d12, int x0, int x1);
 
 private:
 	unsigned int num;//表示规则编号
@@ -25,9 +25,8 @@ private:
 	rule* next;
 	rule* next_arr[101];//用于分类后的链表连接
 };
-rule* h[101];//全局变量，用于存储分类后的各类规则集头结点
-rule* point[101];
-rule* pr[101];
+class rule* pr[101];//用于存储分类后的各类规则集上一个结点
+class rule* head;
 int rule::in(ifstream& fp_rule)
 {
 	if (fp_rule >> x >> ip01 >> x >> ip02 >> x >> ip03 >> x >> ip04 >> x >> ip0wei >> ip11 >> x >> ip12 >> x >> ip13 >> x >> ip14 >> x >> ip1wei >> d01 >> x >> d02 >> d11 >> x >> d12 >> x >> x >> z0[0] >> z0[1] >> x >> x >> x >> z1[0] >> z1[1])return 1;
@@ -35,12 +34,16 @@ int rule::in(ifstream& fp_rule)
 }
 class rule* rule::create_chain_table(ifstream& fp_rule)
 {
-
-	rule* head, * p, * prev;
+	rule* p, * prev;
 	unsigned int number = 0;
 	head = new(rule);
 	head->next = NULL;
 	prev = head;
+	for (int i = 0; i < 101; i++)
+	{
+		head->next_arr[i] = NULL;
+		pr[i] = head;
+	}
 	do
 	{
 		p = new(rule);
@@ -62,23 +65,51 @@ void rule::process_chain_table(rule* p)//访问并处理规则集链表数据
 		IP_Switch_From_CIDR_To_DEC_IP_address_block_Max_and_Min(p->ip0bin, p->ip0wei, &p->ip0max, &p->ip0min);//将源IP地址的CIDR地址块的最大IP地址和最小IP地址分别用十进制表示方式存储在链表的相应变量中
 		IP_Switch_From_CIDR_To_DEC_IP_address_block_Max_and_Min(p->ip1bin, p->ip1wei, &p->ip1max, &p->ip1min);//将目的IP地址的CIDR地址块的最大IP地址和最小IP地址分别用十进制表示方式存储在链表的相应变量中
 		Protocol_number_Switch_From_HEX_To_DEC(&p->x0, &p->x1, p->z0, p->z1);//将协议号上下范围分别转换为十进制表示方式存储到链表相应变量中
-		Process_Rule_class(p, p->ip0min, p->ip0max, p->ip1min, p->ip1max, p->d01, p->d02, p->d11, p->d12, p->x0, p->x1);
+		process_rule_class(&p, p->ip0min, p->ip0max, p->ip1min, p->ip1max, p->d01, p->d02, p->d11, p->d12, p->x0, p->x1);
 		p = p->next;
 	}
 }
+void rule::process_rule_class(class rule** p, unsigned int ip0min, unsigned int ip0max, unsigned int ip1min, unsigned int ip1max, int d01, int d02, int d11, int d12, int x0, int x1)
+{
+	int oz;
+	if (d11 >= 0 && d11 <= 50 && d12 >= 0 && d12 <= 50)oz = 2;
+	else if (d11 >= 51 && d11 <= 100 && d12 >= 51 && d12 <= 100)oz = 4;
+	else if (d11 >= 101 && d11 <= 150 && d12 >= 101 && d12 <= 150)oz = 6;
+	else if (d11 >= 151 && d11 <= 200 && d12 >= 151 && d12 <= 200)oz = 8;
+	else oz = 10;
+	(*p)->next_arr[oz] = NULL;
+	pr[oz]->next_arr[oz] = *p;
+	pr[oz] = *p;
+}
 void rule::visit_processed_chain_table(rule* p, ifstream& fp_packet, ofstream& fp_out)
 {
-	unsigned int ip0, ip1, d0, d1, y, flag;
-	rule* head = p;//存放第一个结点的指针
+	unsigned int ip0, ip1, d0, d1, y, flag, oz;
 	while (fp_packet >> ip0 >> ip1 >> d0 >> d1 >> y)
 	{
-		for (p = head, flag = 0; p->next; p = p->next)
+		if (d1 >= 0 && d1 <= 50)oz = 2;
+		else if (d1 <= 100)oz = 4;
+		else if (d1 <= 150)oz = 6;
+		else if (d1 <= 200)oz = 8;
+		else oz = 10;
+		for (p = head, flag = 0; p; p = p->next_arr[oz])
 		{
 			if (rule_match(ip0, ip1, d0, d1, y, p->ip0min, p->ip0max, p->ip1min, p->ip1max, p->d01, p->d02, p->d11, p->d12, p->x0, p->x1))
 			{
 				fp_out << p->num << endl;
 				flag = 1;
 				break;
+			}
+		}
+		if (!flag)
+		{
+			for (p = head, flag = 0; p; p = p->next_arr[10])
+			{
+				if (rule_match(ip0, ip1, d0, d1, y, p->ip0min, p->ip0max, p->ip1min, p->ip1max, p->d01, p->d02, p->d11, p->d12, p->x0, p->x1))
+				{
+					fp_out << p->num << endl;
+					flag = 1;
+					break;
+				}
 			}
 		}
 		if (!flag)fp_out << "-1\n";
