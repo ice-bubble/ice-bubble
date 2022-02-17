749c0378175a04711be036146fe2880f2538b425
ice-bubble <ice_snow@ice-bubble.com>
2022-02-17 16:24:40 +0800

    Update Process_Rule_class.cpp

--- FZU_OOP_2022_Homework_3/Core Programs/Process_Rule_class.cpp
+++ FZU_OOP_2022_Homework_3/Core Programs/Process_Rule_class.cpp
@@ -18,12 +18,66 @@ class rule* rule::Process_Rule_class(rule* p, unsigned int ip0min, unsigned int 
 			pr[2]->next_arr[2] = p;
 			pr[2] = p;
 		}
+		else if (d11 <= 100)
+		{
+			p->next_arr[4] = NULL;
+			pr[4]->next_arr[4] = p;
+			pr[4] = p;
+		}
+		else if (d11 <= 150)
+		{
+			p->next_arr[6] = NULL;
+			pr[6]->next_arr[6] = p;
+			pr[6] = p;
+		}
+		else if (d11 <= 200)
+		{
+			p->next_arr[8] = NULL;
+			pr[8]->next_arr[8] = p;
+			pr[8] = p;
+		}
+		else
+		{
+			p->next_arr[10] = NULL;
+			pr[10]->next_arr[10] = p;
+			pr[10] = p;
+		}
 	}
 	else
 	{
 		p->next_arr[1] = NULL;
 		pr[1]->next_arr[1] = p;
 		pr[1] = p;
+		if (d11 >= 0 && d11 <= 50)
+		{
+			p->next_arr[3] = NULL;
+			pr[3]->next_arr[3] = p;
+			pr[3] = p;
+		}
+		else if (d11 <= 100)
+		{
+			p->next_arr[5] = NULL;
+			pr[5]->next_arr[5] = p;
+			pr[5] = p;
+		}
+		else if (d11 <= 150)
+		{
+			p->next_arr[7] = NULL;
+			pr[7]->next_arr[7] = p;
+			pr[7] = p;
+		}
+		else if (d11 <= 200)
+		{
+			p->next_arr[9] = NULL;
+			pr[9]->next_arr[9] = p;
+			pr[9] = p;
+		}
+		else
+		{
+			p->next_arr[11] = NULL;
+			pr[11]->next_arr[11] = p;
+			pr[11] = p;
+		}
 	}
 
 }
